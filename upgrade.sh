7c70edfb1a611904bbaa525ead06045bc3fccad8
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2025-01-26 10:59:52 +0530

    Installation and upgrade script related changes.
    Signed-off-by: Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,12 @@
+php artisan cache:clear
+php artisan config:clear
+php artisan event:clear
+php artisan route:clear
+
+git pull
+composer update
 composer install --optimize-autoloader --no-dev
-php artisan migrate
-php artisan key:generate
+
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
